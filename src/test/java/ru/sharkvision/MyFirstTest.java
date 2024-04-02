a9f18e7837778d1a7acc1d1656ab490cbe6cc84c
Michael <m.tideus@gmail.com>
2024-04-03 02:39:36 +0500

    Clear import

--- src/test/java/ru/sharkvision/MyFirstTest.java
+++ src/test/java/ru/sharkvision/MyFirstTest.java
@@ -1,10 +1,6 @@
 package ru.sharkvision;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyFirstTest extends TestBase {
 
